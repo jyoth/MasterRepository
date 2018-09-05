db7ae24b6c0be192d98240c8b5cee00aaf78fec1
Jyo Mithun <jyo.mith@admincontrol.com>
2018-09-05 13:12:27 +0200

    committed

---
+++ MasterFile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MasterFile
+//
+//  Created by Jyo Mithun on 05/09/2018.
+//  Copyright © 2018 Jyo Mithun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	_textField.text = @"It is a master file";
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+
+
+@end
